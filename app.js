296296e835e20aa7d1731d8ec27afe590b60d500
Ryyan <ryyankhan@gmail.com>
2026-02-16 16:01:14 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello Ryyan! Node app running on EC2 with Docker 🚀");
+});
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
+
