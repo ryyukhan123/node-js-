1d7c84da91e90c9fe9074624990cb28109d36591
Ubuntu <ubuntu@ip-172-31-27-196.ec2.internal>
2026-02-18 21:56:56 +0000

    some change sagain done

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello Ryyan! Node app running on EC2 hello ji me ryyan khan shahb and  with Docker 🚀");
+  res.send("Hello Ryyan! Node app running on EC2 hello ji me ryyan ji with kubernetes 🚀");
 });
 
 app.listen(3000, () => {
